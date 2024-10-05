1794ab385df6b117d55837d9dd356855f0a6be01
Yemi Oduyemi <yhermii@gmail.com>
2024-10-05 20:22:13 +0100

    updated

---
+++ index.js
@@ -0,0 +1,2 @@
+const binding = require('./build/Release/binding');
+console.log('Result of add(2, 3):', binding.add(2, 3));
--- src/binding.cc
+++ src/binding.cc
@@ -1,16 +1,32 @@
 #include <nan.h>
 
 void Add(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  double value1 = info[0]->NumberValue(Nan::GetCurrentContext()).FromJust();
-  double value2 = info[1]->NumberValue(Nan::GetCurrentContext()).FromJust();
-  v8::Local<v8::Number> num = Nan::New(value1 + value2);
+    // Check the number of arguments
+    if (info.Length() < 2) {
+        Nan::ThrowTypeError("Two arguments required");
+        return;
+    }
 
-  info.GetReturnValue().Set(num);
+    // Ensure arguments are numbers
+    if (!info[0]->IsNumber() || !info[1]->IsNumber()) {
+        Nan::ThrowTypeError("Both arguments must be numbers");
+        return;
+    }
+
+    double value1 = info[0]->NumberValue(Nan::GetCurrentContext()).FromJust();
+    double value2 = info[1]->NumberValue(Nan::GetCurrentContext()).FromJust();
+    v8::Local<v8::Number> num = Nan::New(value1 + value2);
+
+    info.GetReturnValue().Set(num);
 }
 
 void Init(v8::Local<v8::Object> exports) {
-  exports->Set(Nan::New("add").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Add)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+    // Get the current context
+    v8::Local<v8::Context> context = Nan::GetCurrentContext();
+
+    // Set the 'add' function in the exports object
+    exports->Set(context, Nan::New("add").ToLocalChecked(),
+        Nan::New<v8::FunctionTemplate>(Add)->GetFunction(context).ToLocalChecked()).FromJust();
 }
 
 NODE_MODULE(binding, Init)
